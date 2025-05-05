df835e601fdccc722d16f445cb1dff2f36aca9d6
Ticii18 <ticianovera22@gmail.com>
2025-05-05 09:46:09 -0300

    Se agrego una linea de codigo donde se muestra cargando pagina web antes de entrar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
 
 export function App() {
-  const [Games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     fetch(
@@ -11,46 +20,54 @@ export function App() {
       .then((data) => setGames(data.results))
       .catch((err) => console.log(err));
   }, []);
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-amber-50 to-amber-100">
-        <h1 className="text-amber-950 text-5xl mt-10 m-10 ">Top 10 games of 2019</h1>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6 px-4 w-full max-w-screen-xl">
-          {Games.map((game) => (
-            <div
-              key={game.slug}
-              className="flex flex-col bg-white shadow-sm border border-slate-200 rounded-lg transition-transform duration-500 hover:-translate-y-5 hover:shadow-lg"
-            >
-              <div className="m-2.5 overflow-hidden rounded-md h-56 flex justify-center items-center">
-                <img
-                  className="w-full h-full object-cover"
-                  src={game.background_image}
-                  alt={`Imagen de ${game.name}`}
-                  loading="lazy"
-                />
-              </div>
-              <div className="p-6 text-center">
-                <h4 className="mb-1 text-xl font-semibold text-slate-800">
-                  {game.name}
-                </h4>
-                <p className="text-sm font-semibold text-slate-500 ">
-                  Lanzamiento : {game.released}
-                </p>
-                <p className="text-base text-slate-600 mt-4 font-light ">
-                  Plataforma :{" "}
-                  {game.platforms
-                    .map((platform) => platform.platform.name)
-                    .join(", ")}
-                </p>
-                <p className="text-base text-slate-600 mt-4 font-light ">
-                  Genero : {game.genres.map((genre) => genre.name).join(", ")}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
+  if (!isLoaded) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-amber-50">
+        <h2 className="text-3xl text-amber-800 font-bold">Cargando pagina web</h2>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-amber-50 to-amber-100">
+      <h1 className="text-amber-950 text-5xl mt-10 m-10">
+        Top games of 2019
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6 px-4 w-full max-w-screen-xl">
+        {games.map((game) => (
+          <div
+            key={game.slug}
+            className="flex flex-col bg-white shadow-sm border border-slate-200 rounded-lg transition-transform duration-500 hover:-translate-y-5 hover:shadow-lg"
+          >
+            <div className="m-2.5 overflow-hidden rounded-md h-56 flex justify-center items-center">
+              <img
+                className="w-full h-full object-cover"
+                src={game.background_image}
+                alt={`Imagen de ${game.name}`}
+              />
+            </div>
+            <div className="p-6 text-center">
+              <h4 className="mb-1 text-xl font-semibold text-slate-800">
+                {game.name}
+              </h4>
+              <p className="text-sm font-semibold text-slate-500">
+                Lanzamiento : {game.released}
+              </p>
+              <p className="text-base text-slate-600 mt-4 font-light">
+                Plataforma :{" "}
+                {game.platforms
+                  .map((platform) => platform.platform.name)
+                  .join(", ")}
+              </p>
+              <p className="text-base text-slate-600 mt-4 font-light">
+                Genero : {game.genres.map((genre) => genre.name).join(", ")}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
